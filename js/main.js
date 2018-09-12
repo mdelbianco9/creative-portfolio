3893955e8b9eeb22411ac7d434d2806151f61f2f
marcus delbianco <mdelbianco9@gmail.com>
2018-09-12 13:39:40 -0700

    Created temp hero section before importing gif. trying to get lower boxes to stay fixed to bottom. Bootstrap doing the job, position fixed doesnt work right.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+var change = document.getElementById("newColor");
+
+function change() {
+	document.getElementById("newColor").style.backgroundColor = "Black";
+};
+
+document.getElementById("clickme").addEventListener('click', function() {
+	document.getElementById("newColor").classList.add("mystyle");
+});
