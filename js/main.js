a67b08b56f57025ad3e12b133829e11e4532ae9f
marcus delbianco <mdelbianco9@gmail.com>
2018-09-16 23:10:57 -0700

    Added transition to page-2 using animate.css and JavaScript.

--- js/main.js
+++ js/main.js
@@ -13,8 +13,14 @@ document.getElementById("clickme").addEventListener('click', function() {
 
 /*   Slide #page-2 to view */
 
+document.getElementById('Layer_1').addEventListener('click', function() {
+	var page_2 = document.getElementById('page-2');
+	let classesToAdd = ["animated", "slideInUp", "page-2-intro"];
+	page_2.classList.add(...classesToAdd);
 
-
+	var home_page = document.getElementById('home-page');
+	home_page.classList.add("display-none");
+});
 
 
 
